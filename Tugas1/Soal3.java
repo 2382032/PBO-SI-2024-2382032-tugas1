ced798bc9f01c51f274d98bce91f22e69ea10a4e
2382032 <2382032@unai.edu>
2024-09-17 22:11:52 +0700

    Tugas1

---
+++ Tugas1/Soal3.java
@@ -0,0 +1,25 @@
+package Tugas1;
+
+public class Soal3 {
+    public static void main(String[] args) {
+        System.out.println(countCameICaseWords("SatuDuaTiga"));
+
+        System.out.println(countCameICaseWords("SaveChangesInEditor"));
+    }
+
+    public static int countCameICaseWords(String camelICaseString) {
+        if (camelICaseString == null || camelICaseString.isEmpty()){
+            return 0;
+        }
+
+        int count = 1;
+        for (char c : camelICaseString.toCharArray()){
+            if (Character.isUpperCase(c)){
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+}
